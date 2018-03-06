ba07477a4bac0c841776fdde9e6f2cadbaf108be
martin parenti <martin.parenti@hotmail.fr>
2018-03-06 18:08:32 +0100

    ajout search icon

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,18 @@
 40.times do |x|
-  Recipe.create!([{
+  recipe = Recipe.create!(
     name: "Pizzetas saucisse, romarin et citron #{x}",
-    # picture: ,
     description: "- Préchauffez le four à 170°C. Effeuillez et hachez leromarin.
     Étalez la pâte à pizzaet découpez 12 ronds de pâte à l’aide d’un petit emporte-pièce.
     Taillezlasaucisseen 12 tranches. Placez une rondelle de saucisse sur chaque pâton et enfournez 25min sur une plaque.
     Sortez du four, saupoudrez de romarinet du zeste du citronrâpé.",
     cooking_time: "25 min",
     preparation_time: "15 min"
-  },
-  {
+  )
+  recipe.remote_picture_url = "https://www.lanutrition.fr/sites/default/files/styles/article_large/public/ressources/shutterstock_417390622_pates_legumes.jpg?itok=TmeXLGus"
+  recipe.save
+
+  recipe = Recipe.create!(
     name: "Sablés au parmesan et olives #{x}",
-    # picture: ,
     description: "Hachez les olives et râpez le parmesan.
     Mélangez le beurre mou avec le parmesan, la farine et les olives hachées en malaxant.
     Formez des boudins et laissez reposer 1h au réfrigérateur.
@@ -20,7 +21,8 @@
     Laissez refroidir avant de les détacher.",
     cooking_time: "15 min",
     preparation_time: "25 min"
-    }
-  ])
+    )
+  recipe.remote_picture_url = "https://www.astucesdefilles.com/wp-content/uploads/2015/08/crevettes1.jpg"
+  recipe.save
 end
 
